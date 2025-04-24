67e418d2cdd05d7cee3492b3b9d3bc68b18996ac
angelina <aangelainis@gmail.com>
2025-04-24 14:52:39 -0300

    Feat #01: Add dependencies and choosed Technology

---
+++ FrontEnd/src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+
+
+export const metadata: Metadata = {
+  title: "Momentus",
+  description: "",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={` antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
