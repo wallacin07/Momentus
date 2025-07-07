c61872ad15cdbf532a74c991e3710bceae2f9197
juanoned <juandiego.jd42@gmail.com>
2025-07-07 13:47:27 -0300

    Fix spelling of 'adress' to 'address' in clients model

--- BackEnd/models/clients.js
+++ BackEnd/models/clients.js
@@ -29,7 +29,7 @@ module.exports= (sequelize) => {
             type:Sequelize.DATE,
             allowNull:false
         },
-        adress:{
+        address:{
             type:Sequelize.STRING,
             allowNull:false
         },
